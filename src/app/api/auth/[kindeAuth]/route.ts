1c0a2c3fdf9e160b7535193cf108fa4d98f23eee
jbresky <joaquin_bresky@hotmail.com>
2023-11-22 20:44:25 -0300

    trying options

--- src/app/api/auth/[kindeAuth]/route.ts
+++ src/app/api/auth/[kindeAuth]/route.ts
@@ -1,6 +1,7 @@
 import { handleAuth } from "@kinde-oss/kinde-auth-nextjs/server";
+import { options } from "./options";
 
-const handler = handleAuth();
+const handler = handleAuth(options);
 
 export { handler as GET }
 
