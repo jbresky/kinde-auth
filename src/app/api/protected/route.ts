56e9e509d570e338677ba0ce276a46f0caa1c07e
jbresky <joaquin_bresky@hotmail.com>
2023-11-22 21:01:49 -0300

    res: any

--- src/app/api/protected/route.ts
+++ src/app/api/protected/route.ts
@@ -8,12 +8,8 @@ export async function GET() {
     return new Response("Unauthorized", { status: 401 });
   }
 
-  const user = await getUser();
-  if (user) {
-    return NextResponse.json({
-      message: "Hello user",
-      id: user.id
-    });
-  }
-  return
+  const user: any = getUser();
+  const data = { message: "Hello User", id: user.id };
+
+  return NextResponse.json({ data });
 }
