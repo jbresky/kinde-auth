02d4a82c89bfd06daefb417ebc5b79dacee45d95
jbresky <joaquin_bresky@hotmail.com>
2023-12-13 18:16:30 -0300

    filtering ticket

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,58 @@
 
 import TicketCard from "./components/ticket-card";
 
-export default function Home() {
+export async function getTickets() {
+  try {
+
+    const res = await fetch("http://localhost:3000/api/Tickets", {
+      cache: "no-store"
+    })
+
+    if (!res.ok) {
+      throw new Error("Failed to fetch topics")
+    }
+
+    return res.json()
+
+  } catch (error) {
+    console.log("Error loading topics: ", error);
+
+  }
+}
+
+export default async function Home() {
+  const data = await getTickets()
+
+  if (!data.tickets) {
+    return <p>No tickets</p>
+  }
+
+  const tickets = data.tickets
+
+  const uniqueCategories = [
+    //@ts-ignore
+    ...new Set(tickets?.map(({ category }: any) => category))
+  ]
+
   return (
     <div className="container">
-      <TicketCard />
-      <TicketCard />
-      <TicketCard />
-      <TicketCard />
+      {
+        tickets && uniqueCategories?.map((uniqueCategory, categoryIndex) => (
+          <div key={categoryIndex}>
+            <h2>{uniqueCategory}</h2>
+            {
+              tickets.filter((ticket: any) => ticket.category === uniqueCategory)
+              .map((filteredTicket: any, _index: any) => (
+                <TicketCard
+                  id={_index}
+                  key={_index} 
+                  ticket={filteredTicket}
+                />
+              ))
+            }
+          </div>
+        ))
+      }
     </div>
   );
 }
