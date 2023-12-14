03aa195b7e841440c0c2cf29c64917bda9fba5b1
jbresky <joaquin_bresky@hotmail.com>
2023-12-14 05:11:39 -0300

    dashboard empty

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -4,10 +4,6 @@ export default function Dashboard() {
   return (
     <div className="px-10">
       <div className="lg:grid grid-cols-2 2xl:grid-cols-4 gap-2">
-      <TicketCard />
-      <TicketCard />
-      <TicketCard />
-      <TicketCard />
       </div>
     </div>
   );
