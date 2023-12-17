39094ab779261a81679adbbf4a06109b44eec182
jbresky <joaquin_bresky@hotmail.com>
2023-12-16 21:52:43 -0300

    width of bar

--- src/app/components/progress.tsx
+++ src/app/components/progress.tsx
@@ -4,7 +4,8 @@ const Progress = ({ progress }: { progress: number }) => {
   return (
     <div className="w-full rounded-full h-2.5 bg-gray-300">
       <div
-        className={`bg-blue-500 h-2.5 rounded-full w-[${progress}%]`}
+        className='bg-blue-500 h-2.5 rounded-full'
+        style={{width: `${progress}%`}}
       ></div>
     </div>
   );
