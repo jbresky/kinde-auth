3ef1956cfca8b7b5c7384e86498bda14a4c47c46
jbresky <joaquin_bresky@hotmail.com>
2023-12-16 21:50:37 -0300

    GET - PUT

---
+++ src/app/api/Tickets/[id]/route.ts
@@ -0,0 +1,27 @@
+import Ticket from "@/models/Ticket";
+import { NextResponse } from "next/server";
+
+export async function GET(req: Request, { params }: any) {
+    const { id } = params
+    const foundTicket = await Ticket.findOne({ _id: id })
+    return NextResponse.json({ foundTicket }, { status: 200 })
+}
+
+export async function PUT(req: Request, { params }: any) {
+    try {
+        const { id } = params
+
+        const body = await req.json()
+        const ticketData = body.formData
+
+        await Ticket.findByIdAndUpdate(id, {
+            ...ticketData
+        })
+
+        return NextResponse.json({ message: "Ticket updated" }, { status: 200 })
+    } catch (error) {
+        console.log(error);
+        return NextResponse.json({ message: "Error", error }, { status: 500 })
+    }
+
+}
