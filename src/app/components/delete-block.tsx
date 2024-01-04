c19bb0694052bbc4ae9d026dcfc421d1df88f91a
jbresky <joaquin_bresky@hotmail.com>
2024-01-04 14:56:10 -0300

    loading state on error

--- src/app/components/delete-block.tsx
+++ src/app/components/delete-block.tsx
@@ -5,24 +5,25 @@ import toast, { Toaster } from 'react-hot-toast'
 import useSWR from 'swr'
 import { useState } from 'react'
 import { ticketsUrlEndpoint as cacheKey, deleteTicket, getTickets } from "@/services/swr/tickets-api";
-import { deleteTicketOptions } from "@/services/swr/tickets-swr-options";
 import { BeatLoader } from "react-spinners";
 
 const DeleteBlock = ({ id }: { id: string }) => {
     const [loading, setLoading] = useState(false)
-    const { mutate, isLoading } = useSWR(cacheKey, getTickets)
+    const { mutate } = useSWR(cacheKey, getTickets)
 
     const deleteTicketMutation = async () => {
         try {
             setLoading(true)
-            await deleteTicket(id)
+            await deleteTicket(id) 
             mutate()
 
             toast.success("Succes! Item deleted", {
                 duration: 1000,
                 icon: '🎉'
             })
+            setLoading(false)
         } catch (error) {
+            setLoading(false)
             toast.error("Failed to delete item", {
                 duration: 1000
             })
@@ -31,7 +32,7 @@ const DeleteBlock = ({ id }: { id: string }) => {
 
     return (
         <>
-        <Toaster toastOptions={{ position: 'bottom-center'}}/>
+            <Toaster toastOptions={{ position: 'bottom-center' }} />
             {loading ? <BeatLoader /> : <AiTwotoneDelete onClick={deleteTicketMutation} />}
         </>
     );
