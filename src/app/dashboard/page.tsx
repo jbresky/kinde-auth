2456254a8eca59f2ccb6604d11f82b270a0c7abc
jbresky <joaquin_bresky@hotmail.com>
2024-01-05 18:27:23 -0300

    user tickets

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,9 +1,27 @@
+'use client'
+
 import TicketCard from "../components/ticket-card";
+import useSWR from 'swr'
+import { getTickets, ticketsUrlEndpoint as cacheKey } from "@/services/swr/tickets-api";
 
 export default function Dashboard() {
+  const { data, isLoading } = useSWR(cacheKey, getTickets)
+  // getTicketsByUser instead
+
   return (
-    <div className="px-10">
-      <div className="lg:grid grid-cols-2 2xl:grid-cols-4 gap-2">
+    <div className="py-4 px-20 2xl:px-32">
+        <h1 className="text-2xl">Your tickets</h1>
+      <div className="lg:grid grid-cols-2 2xl:grid-cols-4 mt-3">
+        {
+          isLoading ? <p>Loading...</p> : (
+            data && data.tickets.map((ticket: any) => (
+              <TicketCard ticket={ticket} />
+            ))
+          )
+        }
+        {
+          data.tickets.length === 0 && <p>You don&apos;t have tickets asigned</p>
+        }
       </div>
     </div>
   );
