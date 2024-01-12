ac147a03bd76e74928266b18cf910261f0c6f2c2
jbresky <joaquin_bresky@hotmail.com>
2024-01-11 22:51:01 -0300

    input & dashboard component

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -4,8 +4,9 @@ import useSWR from 'swr'
 import { getTickets, ticketsUrlEndpoint as cacheKey } from "@/services/swr/tickets-api";
 import { BeatLoader } from "react-spinners";
 import FilteredTickets from "../components/tickets/filtered-tickets";
+import Dashboard from '../components/dashboard/dashboard';
 
-export default function Dashboard() {
+export default function DashboardPage() {
 
   const { data, isLoading } = useSWR(cacheKey, getTickets)
   // getTicketsByUser instead
@@ -21,11 +22,18 @@ export default function Dashboard() {
     content = <div className="p-24"> <BeatLoader color="gray" /> </div>
   } else {
     content = (
-      <main className="mb-6 px-20">
-        <h1 className="text-2xl mt-4">Your tickets</h1>
+      <main className="mb-6 px-2 md:px-20">
+        <div className='text-right mb-6'>
+          <input 
+          className='p-2 rounded-lg bg-slate-100 placeholder:text-gray-600 placeholder:text-sm text-sm focus:outline-none focus:ring focus:ring-gray-600'
+          placeholder='Search by title..'/>
+        </div>        
+        <Dashboard />
         <FilteredTickets
-          data={data} 
+          data={data}
           uniqueCategories={uniqueCategories} />
+        {console.log(data.tickets)
+        }          
         {
           data && data.tickets.length === 0 && <p>You don&apos;t have tickets asigned</p>
         }
