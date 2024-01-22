45b5f523dca47372f044ada9a758282b2cd356bd
jbresky <joaquin_bresky@hotmail.com>
2024-01-22 08:43:32 -0300

    moving to top - more minimalist

--- src/app/components/tickets/create-ticket.tsx
+++ src/app/components/tickets/create-ticket.tsx
@@ -5,9 +5,8 @@ import { IoMdAdd } from "react-icons/io";
 
 const CreateTicket = () => {
     return (
-        <Link href='/ticket-page/new' className="w-full 2xl:w-1/4 h-[170px] flex items-center justify-center gap-2 font-sans font-semibold border-2 border-gray-300 bg-slate-100/40 rounded-xl hover:shadow-sm transition duration-200 p-4 mt-2">
+        <Link href='/ticket-page/new' className="flex items-center justify-center gap-2 rounded-full border-2 border-gray-300 font-sans font-medium p-2">
                 <IoMdAdd />
-                <h1>Add new ticket</h1>
         </Link>
     );
 }
