26bd4f24d81e528cc1cd7965a758e6eb0afa4a75
jbresky <joaquin_bresky@hotmail.com>
2024-01-22 08:44:34 -0300

    padding x

--- src/app/components/tickets/filtered-tickets.tsx
+++ src/app/components/tickets/filtered-tickets.tsx
@@ -14,7 +14,7 @@ const FilteredTickets = ({ data, uniqueCategories }: FilterProps) => {
             data && data.tickets && uniqueCategories?.map((uniqueCategory, categoryIndex: number) => (
                 <div key={categoryIndex} className="mb-4">
                     <h2 className="text-slate-500 font-sans font-medium">{uniqueCategory}</h2>
-                    <div className="lg:grid grid-cols-2 2xl:grid-cols-4 gap-2">
+                    <div className="lg:grid grid-cols-2 2xl:grid-cols-4 gap-2 flex flex-col max-lg:gap-4">
                         {
                             data && data.tickets.filter((ticket: Ticket) => ticket.category === uniqueCategory)
                                 .map((filteredTicket: Ticket, _index: any) => (
