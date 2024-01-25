a39d72bdc6f4a7eef7a5485c0a8d098532eab6e3
jbresky <joaquin_bresky@hotmail.com>
2024-01-25 08:00:12 -0300

    search ticket

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -5,6 +5,7 @@ import { getTickets, ticketsUrlEndpoint as cacheKey } from "@/services/swr/ticke
 import { BeatLoader } from "react-spinners";
 import FilteredTickets from "../components/tickets/filtered-tickets";
 import Dashboard from '../components/dashboard/dashboard';
+import SearchTicket from '../components/searchTicket';
 
 export default function DashboardPage() {
 
@@ -24,19 +25,12 @@ export default function DashboardPage() {
     content = (
       <main className="mb-6 px-2 md:px-20">
         <div className='text-right mb-6'>
-          <input 
-          className='p-2 rounded-lg bg-slate-100 placeholder:text-gray-600 placeholder:text-sm text-sm focus:outline-none focus:ring focus:ring-gray-600'
-          placeholder='Search by title..'/>
-        </div>        
+          <SearchTicket />
+        </div>
         <Dashboard />
         <FilteredTickets
           data={data}
           uniqueCategories={uniqueCategories} />
-        {console.log(data.tickets)
-        }          
-        {
-          data && data.tickets.length === 0 && <p>You don&apos;t have tickets asigned</p>
-        }
       </main>
     )
   }
