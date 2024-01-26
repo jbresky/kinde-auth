a7cd602a57d8d5239e059c3bb6b921936cee8120
jbresky <joaquin_bresky@hotmail.com>
2024-01-26 15:30:33 -0300

    conditional data: filter within component

--- src/app/components/tickets/filtered-tickets.tsx
+++ src/app/components/tickets/filtered-tickets.tsx
@@ -1,35 +1,36 @@
 import TicketCard from "./ticket-card";
 
-interface FilterProps {
-    data: any,
-    uniqueCategories: string[],
-}
+const FilteredTickets = ({ data }: { data: any }) => {
 
-const FilteredTickets = ({ data, uniqueCategories }: FilterProps) => {
+    const uniqueCategories = [
+        //@ts-ignore
+        ...new Set(data && data.tickets?.map(({ category }: { category: string }) => category))
+    ]
+
     return (
         <section className="mt-8">
-        {
-            // filtering tickets by Category
+            {
+                // filtering tickets by Category
 
-            data && data.tickets && uniqueCategories?.map((uniqueCategory, categoryIndex: number) => (
-                <div key={categoryIndex} className="mb-4">
-                    <h2 className="text-slate-500 font-sans font-medium">{uniqueCategory}</h2>
-                    <div className="lg:grid grid-cols-2 2xl:grid-cols-4 gap-2 flex flex-col max-lg:gap-4">
-                        {
-                            data && data.tickets.filter((ticket: Ticket) => ticket.category === uniqueCategory)
-                                .map((filteredTicket: Ticket, _index: any) => (
-                                    <TicketCard
-                                        key={_index}
-                                        ticket={filteredTicket}
-                                    />
-                                ))
-                        }
+                data && data.tickets && uniqueCategories?.map((uniqueCategory, categoryIndex: number) => (
+                    <div key={categoryIndex} className="mb-4">
+                        <h2 className="text-slate-500 font-sans font-medium">{uniqueCategory}</h2>
+                        <div className="lg:grid grid-cols-2 2xl:grid-cols-4 gap-2 flex flex-col max-lg:gap-4">
+                            {
+                                data && data.tickets.filter((ticket: Ticket) => ticket.category === uniqueCategory)
+                                    .map((filteredTicket: Ticket, _index: any) => (
+                                        <TicketCard
+                                            key={_index}
+                                            ticket={filteredTicket}
+                                        />
+                                    ))
+                            }
+                        </div>
                     </div>
-                </div>
-            ))
-          }
-          </section>
-     );
+                ))
+            }
+        </section>
+    );
 }
 
 export default FilteredTickets;
