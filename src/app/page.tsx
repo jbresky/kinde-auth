dc11409c7a460f81e55812f6270f886a1c8b8046
jbresky <joaquin_bresky@hotmail.com>
2024-02-01 16:52:41 -0300

    .

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,8 +6,9 @@ import { getTickets, ticketsUrlEndpoint as cacheKey } from "@/services/swr/ticke
 import { BeatLoader } from "react-spinners";
 import FilteredTickets from "./components/tickets/filtered-tickets";
 import SearchTicket from "./components/searchTicket";
-import { BASE_URL } from "@/constant/url";
+// import { BASE_URL } from "@/constant/url";
 
+const BASE_URL = process.env.NODE_ENV == 'production' ? 'https://kinde-auth-sand.vercel.app' : 'http://localhost:3000'
 export const getByTitle = async (title: Ticket["title"]) => {
   const tickets = await fetch(`${BASE_URL}/api/Tickets/search-by-title/${title}`)
 
