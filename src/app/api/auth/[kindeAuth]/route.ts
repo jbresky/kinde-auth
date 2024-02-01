bce490da21eb1d070bf257d0f484d0774a8047be
jbresky <joaquin_bresky@hotmail.com>
2024-02-01 16:36:19 -0300

    props to handler

--- src/app/api/auth/[kindeAuth]/route.ts
+++ src/app/api/auth/[kindeAuth]/route.ts
@@ -1,7 +1,13 @@
 import { NextRequest } from "next/server";
 import { handleAuth } from "@kinde-oss/kinde-auth-nextjs/server";
 
-export async function GET(request: NextRequest, { params }: any) {
+interface Props {
+    params: {
+        kindeAuth: string
+    }
+}
+
+export async function GET(request: NextRequest, { params }: Props) {
     const endpoint = params.kindeAuth
     return handleAuth(request, endpoint)
 }
